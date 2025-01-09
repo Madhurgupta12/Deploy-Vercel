06c6d97daa7c1b5ba2a935c7f13ff789dd2cf57b
Madhurgupta12 <om96002@gmail.com>
2025-01-09 22:59:42 +0530

    Push code

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./index.css"
+const App = () => {
+  return (
+    <div>
+      <center><h1>Hello My name is Madhur Gupta</h1></center>
+    </div>
+  )
+}
+
+export default App
